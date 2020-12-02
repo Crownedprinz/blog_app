72378921dd04a808193561efc9e4c649ca695989
Ademola John <AdemolaJhon@gmail.com>
2020-12-03 00:17:34 +0100

    ammended fonts, and the look of ListView in home page

--- lib/screens/ShowTimer.dart
+++ lib/screens/ShowTimer.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class ShowTimer extends StatelessWidget{
   @override
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return   Row(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        RichText(
-          text: TextSpan(
+        Container(
+          child: Row(
+            mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
             children: [
-              WidgetSpan(
-                child: Icon(Icons.timer, size: 14),
+              Icon(
+                Feather.clock,
+                size: 16,
+                color: Color(0xff9fa0a9),
               ),
-              TextSpan(
-                  text: "58m ago",
-                  style: TextStyle(color: Colors.black)),
+              SizedBox(width: 9),
+              Text(
+                '50m ago',
+                style: TextStyle(
+                    color: Color(0xff9fa0a9), fontSize: 15),
+              )
             ],
           ),
         ),
-        SizedBox(width: 15,),
-        RichText(
-          text: TextSpan(
+        SizedBox(width: 15),
+        Container(
+          child: Row(
+            mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
             children: [
-              WidgetSpan(
-                child: Icon(Icons.add_comment_rounded, size: 14),
+              Icon(
+                Feather.message_circle,
+                size: 16,
+                color: Color(0xff9fa0a9),
               ),
-              TextSpan(
-                  text: "68 comments",
-                  style: TextStyle(color: Colors.black,)),
+              SizedBox(width: 9),
+              Text(
+                '68 comments',
+                style: TextStyle(
+                    color: Color(0xff9fa0a9), fontSize: 15),
+              )
             ],
           ),
-        ),
+        )
       ],
     );
   }
